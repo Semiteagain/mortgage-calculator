449648f7b8dc5e25b6a8e584c107a18ed8b8fb65
Mukaila Semiu <semiteagain@gmail.com>
2021-01-30 16:14:42 +0100

    add heading

---
+++ src/components/Purchase.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+function Purchase(params) {
+    return(
+        <div>
+         <h3>Purchase price: #450000</h3>
+         <h3>Equity:135000</h3>
+         <h3>Repayment time:25years</h3>
+         <h3>Loan Amount: 315000</h3>
+
+         <h3>Estimated per Month: 1300</h3>
+
+
+
+        </div>
+    )
+}
+
+
+
+export default Purchase;
